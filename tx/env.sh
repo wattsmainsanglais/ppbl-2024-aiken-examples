8756a65e9afb23e0f7ed0c9a1c21c06a07c9815e
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-11-22 15:23:05 +0100

    change env.sh

--- tx/env.sh
+++ tx/env.sh
@@ -1,7 +1,7 @@
 # Your cardano-cli wallet address (as created in Lesson 102.1):
 sender=addr_test1vqqxcn992d3c9vvuzzc59zu9hz3rshus4juwwg8luh5tx0q0hh5s4
 # The path to your cardano-cli signing key (as created in Lesson 102.1):
-sender_key=ppbl-2024-cli-wallet/payment.skey
+sender_key=payment.skey
 
 # address where you will create reference UTxOs:
 reference_scripts_addr=
