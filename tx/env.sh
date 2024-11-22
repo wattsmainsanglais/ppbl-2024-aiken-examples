ea91903b12616efdd3372f6cc30a559efd7159b3
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-11-22 15:32:22 +0100

    update env.sh again

--- tx/env.sh
+++ tx/env.sh
@@ -4,7 +4,7 @@ sender=addr_test1vqqxcn992d3c9vvuzzc59zu9hz3rshus4juwwg8luh5tx0q0hh5s4
 sender_key=payment.skey
 
 # address where you will create reference UTxOs:
-reference_scripts_addr=
+reference_scripts_addr=addr_test1wqlf3rtpxy0pk5yumt3uceyjyfhk38690vh8zhdmez0am5s0hmzmg
 
 # Path to signing key associated with reference_scripts_addr (optional):
 reference_scripts_skey=
