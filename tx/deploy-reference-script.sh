9f434a2699042d955d4023c8b9c1eb02ca9418e0
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-11-22 15:06:08 +0100

    expose addr

--- tx/deploy-reference-script.sh
+++ tx/deploy-reference-script.sh
@@ -5,9 +5,10 @@
 
 validator_path=$1
 min_utxo_lovelace=$2
-
-reference_address=addr_test1wqlf3rtpxy0pk5yumt3uceyjyfhk38690vh8zhdmez0am5s0hmzmg
+sender=addr_test1vqqxcn992d3c9vvuzzc59zu9hz3rshus4juwwg8luh5tx0q0hh5s4
 sender_key=payment.skey
+reference_address=addr_test1wqlf3rtpxy0pk5yumt3uceyjyfhk38690vh8zhdmez0am5s0hmzmg
+
 
 tx_in_fees=$(get_address_biggest_lovelace ${sender})
 
