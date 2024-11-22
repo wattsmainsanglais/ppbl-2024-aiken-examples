13af433401b4cfdaf2962dc07d7f906a276a00b6
wattsmainsanglais <wattsmainsanglais@gmail.com>
2024-11-22 15:21:27 +0100

    chnage deploy script

--- tx/deploy-reference-script.sh
+++ tx/deploy-reference-script.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
 . env.sh
@@ -5,27 +6,26 @@
 
 validator_path=$1
 min_utxo_lovelace=$2
-sender=addr_test1vqqxcn992d3c9vvuzzc59zu9hz3rshus4juwwg8luh5tx0q0hh5s4
-sender_key=payment.skey
-reference_address=addr_test1wqlf3rtpxy0pk5yumt3uceyjyfhk38690vh8zhdmez0am5s0hmzmg
-
 
 tx_in_fees=$(get_address_biggest_lovelace ${sender})
 
 cardano-cli conway transaction build \
---testnet-magic 1 \
---tx-in $tx_in_fees \
---tx-out $reference_address+$min_utxo_lovelace \
---tx-out-reference-script-file $validator_path \
---change-address $sender \
---out-file deploy-reference-script.draft
+  --testnet-magic 1 \
+  --tx-in $tx_in_fees \
+  --tx-out $reference_scripts_addr+$min_utxo_lovelace \
+  --tx-out-reference-script-file $validator_path \
+  --change-address $sender \
+  --out-file deploy-reference-script.draft
 
 cardano-cli conway transaction sign \
---signing-key-file $sender_key \
---testnet-magic 1 \
---tx-body-file deploy-reference-script.draft \
---out-file deploy-reference-script.signed
+  --signing-key-file $sender_key \
+  --testnet-magic 1 \
+  --tx-body-file deploy-reference-script.draft \
+  --out-file deploy-reference-script.signed
 
 cardano-cli conway transaction submit \
---tx-file deploy-reference-script.signed \
---testnet-magic 1
+  --tx-file deploy-reference-script.signed \
+  --testnet-magic 1
+
+echo "Successful txHash:"
+echo $(cardano-cli conway transaction txid --tx-file deploy-reference-script.signed)
